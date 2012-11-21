cdfc81602e0d504e27f11970898bd4b9cc750227
Rick Cabrera <rick@findexp.com>
2012-11-21 11:18:59 -0500

    - Send deployment message to FlowDock when build is finished

--- bin/common.sh
+++ bin/common.sh
@@ -12,3 +12,30 @@ get_property()
   grep -E ^$escaped_key[[:space:]=]+ $file | \
   sed -E -e "s/$escaped_key([\ \t]*=[\ \t]*|[\ \t]+)([A-Za-z0-9\.-]*).*/\2/g"
 }
+
+
+send_deploy_notification() {
+	PROJECT=$1
+	SUBJECT=$2
+	INFO=$3
+	
+	URL='https://api.flowdock.com/messages/team_inbox/418e0cc9c040bcdeefcad45c7aa2a034'
+	
+	CONTENT="<html><body><h1>$PROJECT</h1><h3>Built and deployed!</h3>\
+<a href=\\\"http://www.youtube.com/watch?v=vCadcBR95oU\\\">\
+<img src=\\\"http://userserve-ak.last.fm/serve/252/23905581.jpg\\\" alt=\\\"Push It Real Good!\\\"/>\
+</a>\
+<div>\
+<h3>Build Information</h3>\
+$INFO\
+</div>\
+</body></html>"
+
+	PAYLOAD="{ \"source\": \"Builder\", \"from_address\": \"bobTheBuilder@findexp.com\", \
+\"subject\": \"$SUBJECT\", \"project\": \"$PROJECT\", \
+\"tags\": [\"#builds\"], \"content\": \"$CONTENT\" }"
+
+	echo "PROJECT: $PROJECT   SUBJECT:  $SUBJECT  PAYLOAD: $PAYLOAD"
+	curl -v -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d "$PAYLOAD" $URL
+}
+
