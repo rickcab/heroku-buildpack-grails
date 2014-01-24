e686ae05290420bbc6415a411b84cba9136e8b18
Rick Cabrera <rick@findexp.com>
2014-01-24 17:21:38 -0500

    Add common export_env_dir function for pulling env vars out of new Heroku env_dir

--- bin/common.sh
+++ bin/common.sh
@@ -41,3 +41,17 @@ $INFO\
 	curl -v -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d "$PAYLOAD" $URL
 }
 
+
+export_env_dir() {
+    env_dir=$1
+    whitelist_regex=${2:-''}
+    blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+    if [ -d "$env_dir" ]; then
+        for e in $(ls $env_dir); do
+            echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+            export "$e=$(cat $env_dir/$e)"
+        :
+        done
+    fi
+}
+
